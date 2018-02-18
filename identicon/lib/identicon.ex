d773071d3e842fd5ad38cf35f794ff824de934c0
Jose <jose.lara.cuadrado@gmail.com>
2018-02-18 13:32:06 +0100

    build_grid function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
         input 
         |> hash_input
         |> pick_color
+        |> build_grid
     end
 
     def hash_input(input) do
@@ -20,6 +21,7 @@ defmodule Identicon do
         
         ##asignamos a la propiedad color de la estructura la tupla con los valores r,g,b
         ## serioa como image.color = {r,g,b} en un lenguaje normal
+        ##hay que poner el image para mantener el valor del hex, si no estarías obteniendo un struct con hex: nil y color {a,b,c}
         %Identicon.Image{image | color: {r, g, b}}
     
     end
@@ -32,5 +34,18 @@ defmodule Identicon do
     ##        b: image.hex[2]
     ##    }
     ##}
+
+    def build_grid(%Identicon.Image{hex: hex} = image) do
+         hex
+         |> Enum.chunk(3) ##Enum.chunk(hex,3)
+         |> Enum.map(&mirror_row/1) # &referencia a una funcion, /1 se refiere a la funcion que tiene un solo un argumento
+ 
+    end
+
+    def mirror_row([first,second | _tail] = row) do
+         #[first, second | _tail] = row
+
+         row ++ [second, first]
+    end
 
 end
