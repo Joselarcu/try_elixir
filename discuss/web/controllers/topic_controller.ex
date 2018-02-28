b08d87da4ff10c8fd09386e793346e351d282fec
Jose <jose.lara.cuadrado@gmail.com>
2018-02-28 21:00:41 +0100

    add topic route and controller

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,9 @@
+defmodule Discuss.TopicController do
+
+    use Discuss.Web, :controller #coge los paquetes que necesita de web.ex de los imports del module controller
+
+    def new(conn, params) do
+     
+    end
+
+end
