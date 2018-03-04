4cefe71a1d150c5b0c03e0155dfd6ea3b7d6f9e8
Jose <jose.lara.cuadrado@gmail.com>
2018-03-04 22:33:57 +0100

    topic create

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,8 +2,27 @@ defmodule Discuss.TopicController do
 
     use Discuss.Web, :controller #coge los paquetes que necesita de web.ex de los imports del module controller
 
-    def new(conn, params) do
-     
+    alias Discuss.Topic
+
+    def new(conn, _params) do
+        changeset = Topic.changeset(%Topic{},%{})
+
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, %{"topic" => topic}) do
+        IO.inspect(topic)
+         #%{"topic" => topic} = params
+         changeset = Topic.changeset(%Topic{},topic)
+        case Repo.insert(changeset) do
+            {:ok,post} -> IO.inspect(post)
+            {:error,changeset} ->
+                render conn, "new.html", changeset: changeset
+        end
+    end
+
+    def index(conn, _params) do
+        render conn, "list.html", changeset: changeset
     end
 
 end
